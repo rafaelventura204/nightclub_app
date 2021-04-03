1a48542912f37ede15c022a20d1b016bf3592f31
rafaelventura <rafaelventura74@gmail.com>
2021-04-03 19:36:50 +0200

    MAPPA

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:bar_pub/models/user.dart';
+  import 'package:bar_pub/models/user.dart';
 import 'package:bar_pub/screens/wrapper.dart';
 import 'package:bar_pub/services/auth.dart';
 import 'package:firebase_core/firebase_core.dart';
--- lib/screens/search.dart
+++ lib/screens/search.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_map_polyline/google_map_polyline.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission/permission.dart';
 
 class MySearch extends StatelessWidget {
   @override
@@ -20,12 +24,93 @@ class MySearchPage extends StatefulWidget {
 }
 
 class _MySearchPageState extends State<MySearchPage> {
+  /*POSIZIONE ATTUALE*/
+  bool mapToggle = false;
+  var currentLocation;
+  GoogleMapController mapController;
+
+  /*POLYLINE */
+  final Set<Polyline> polyline = {};
+  //Set di coordinalte
+  List<LatLng> routeCoords;
+  GoogleMapPolyline googleMapPolyline =
+      new GoogleMapPolyline(apiKey: "AIzaSyDlitorRQ9IutFGY-WZCWu65PmjHdpqD8U");
+
+  getSomePoints() async {
+    var permissions =
+        await Permission.getPermissionsStatus([PermissionName.Location]);
+    if (permissions[0].permissionStatus == PermissionStatus.notAgain) {
+      var askpermission =
+          await Permission.requestPermissions([PermissionName.Location]);
+    } else {
+      routeCoords = await googleMapPolyline.getCoordinatesWithLocation(
+          origin: LatLng(currentLocation.latitude, currentLocation.longitude),
+          destination: LatLng(45.432083533092786, 9.19928841039524),
+          mode: RouteMode.driving);
+    }
+  }
+
+  void initState() {
+    super.initState();
+    Geolocator.getCurrentPosition().then((currloc) {
+      setState(() {
+        currentLocation = currloc;
+        mapToggle = true;
+      });
+    });
+    getSomePoints();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Text('CERCA LOCALI'),
-      backgroundColor: Colors.purple[300],
-    ));
+      appBar: AppBar(
+        title: Text('CERCA LOCALI'),
+        backgroundColor: Colors.purple[300],
+      ),
+      body: Column(
+        children: <Widget>[
+          Stack(
+            children: <Widget>[
+              Container(
+                height: MediaQuery.of(context).size.height - 141.0,
+                width: double.infinity,
+                child: mapToggle
+                    ? GoogleMap(
+                        onMapCreated: onMapCreated,
+                        polylines: polyline,
+                        initialCameraPosition: CameraPosition(
+                            //NEL TARGET è possibile passare un punto diverso
+                            target: LatLng(currentLocation.latitude,
+                                currentLocation.longitude),
+                            zoom: 15.0),
+                        mapType: MapType.normal,
+                      )
+                    : Center(
+                        child: Text(
+                        'Caricamento in corso...',
+                        style: TextStyle(fontSize: 20.0),
+                      )),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  void onMapCreated(controller) {
+    setState(() {
+      mapController = controller;
+
+      polyline.add(Polyline(
+          polylineId: PolylineId('route1'),
+          visible: true,
+          points: routeCoords,
+          width: 4,
+          color: Colors.purple[300],
+          startCap: Cap.roundCap,
+          endCap: Cap.buttCap));
+    });
   }
 }
